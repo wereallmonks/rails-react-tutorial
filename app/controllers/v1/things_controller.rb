8cc1227727bfe9c3c0c5e8b5234cc7bd7a90cf0f
Claudia Vizena <cvizena@Claudias-MacBook-Pro.local>
2020-03-06 14:16:04 -0800

    Create new view index.html

--- app/controllers/v1/things_controller.rb
+++ app/controllers/v1/things_controller.rb
@@ -1,7 +1,6 @@
 class V1::ThingsController < ApplicationController
   def index
-    render json: {
-      :things => [
+    render json: { :things => [
         {
           :name => 'some-thing',
           :guid => '23545100-5e7e-11ea-ab12-0800200c9a66'
