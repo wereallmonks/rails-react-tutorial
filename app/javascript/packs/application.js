edb4b1ee0ca989cac8e5e638e1cdaf0ab31e4629
Claudia Vizena <cvizena@Claudias-MacBook-Pro.local>
2020-03-09 11:55:06 -0700

    Install react router.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
+require("@rails/ujs")
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
