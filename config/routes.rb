096eec3520f031f2e0d373d6d7ce00d4d439d17e
Claudia Vizena <cvizena@Claudias-MacBook-Pro.local>
2020-03-10 14:29:18 -0700

    Add an Action and a Reducer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  root 'static#index'
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
+  end
+  root 'static#index'
+  # Forward all requests to StaticController#index but requests
+  # must be non-Ajax (!req.xhr?) and HTML Mime type (req.format.html?).
+  # This does not include the root ("/") path.
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
   end
 end
