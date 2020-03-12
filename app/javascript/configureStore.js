e5e8f7cccf560abbecc6a07bcfaf3a2bcf6d440f
Claudia Vizena <cvizena@Claudias-MacBook-Pro.local>
2020-03-12 11:11:10 -0700

    Add Semantic UI and key to elements.

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,6 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 
 import thunk from 'redux-thunk'
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 const initialState = {
   things: [
     {
