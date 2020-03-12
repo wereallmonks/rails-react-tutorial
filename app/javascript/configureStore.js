8f425b2669769c79cfc4fb8c652ec864aeae4237
Claudia Vizena <cvizena@Claudias-MacBook-Pro.local>
2020-03-11 17:28:52 -0700

    Install Redux Dev Tools.

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,18 +1,33 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
+import thunk from 'redux-thunk'
 const initialState = {
-  things: []
+  things: [
+    {
+      name: "test",
+      guid: "123"
+    }
+  ]
 };
 
 function rootReducer(state, action) {
   console.log(action.type);
   switch (action.type) {
-    default:
-      return state
+    case "GET_THINGS_SUCCESS":
+      return { things: action.json.things };
   }
+  return state;
 }
 
 export default function configureStore() {
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(
+      applyMiddleware(
+        thunk,
+      )
+    )
+  );
   return store;
 }
